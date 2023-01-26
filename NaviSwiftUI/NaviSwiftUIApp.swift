94827038119c8502a037f064ada0c01b8411c851
Amit Kumar <amitsangwan898@gmail.com>
2023-01-26 10:05:47 +0530

    Started Navi

---
+++ NaviSwiftUI/NaviSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NaviSwiftUIApp.swift
+//  NaviSwiftUI
+//
+//  Created by Amit Kumar on 26/01/23.
+//
+
+import SwiftUI
+
+@main
+struct NaviSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
